991df9da0f20f8b25a0683a83acb3e22252c342e
Javier López <javierpitino@gmail.com>
2022-05-10 16:48:41 +0200

    iteracion 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,36 @@
+const CelebrityModel = require("../models/Celebrity.model");
+
 const router = require("express").Router();
 
+router.get("/", (req, res, next) => {
+    res.render("celebrities/celebrities.hbs")
+})
 
+router.get("/create", (req, res, next) => {
+
+   res.render("celebrities/new-celebrity.hbs")
+
+})
+
+router.post("/create", async (req, res, next) => {
+
+    const { name, occupation, catchPhrase } = req.body
+    try {
+        
+        await CelebrityModel.create({
+            name,
+            occupation,
+            catchPhrase
+        })
+
+        res.redirect("/celebrities")
+
+    } catch(err) {
+        res.render("celebrities/new-celebrity.hbs")
+    }
+
+
+})
 
 
 
